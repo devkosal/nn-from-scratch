7fb5884a2566ce70964bccc8dd5a6a246ff704d6
devkosal <devkosal@gmail.com>
2019-09-11 13:11:15 -0400

    added intermediate"

--- nn_from_scratch_advanced.py
+++ nn_from_scratch_advanced.py
@@ -29,10 +29,6 @@ def rmse(input,pred):
     mse = ((pred-input)**2).mean()
     return np.sqrt(mse)
 
-#
-# x = np.random.randn(100,3)
-#
-# y = sigmoid(np.array([x[:,0]**2 + x[:,1]**4 - x[:,2]**3]).T)
 
 
 # Airbnb
@@ -234,8 +230,6 @@ final = pd.DataFrame({"id": sub_data["id"],"price":sub_pred.ravel()})
 
 final.to_csv("late_submission.csv",index=False)
 
-! kaggle competitions submit -c airbnblala1 -f late_submission.csv -m "Message"
-
 #Airbnb benchmark
 
 from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
@@ -257,46 +251,3 @@ print('Random Forest Test RMSE: %.4f' % forest_rmse)
 for i in range(27):
     if data.drop("price",axis=1).columns[i] != sub_data.columns[i]:
         print(i)
-
-# Original Code
-
-import numpy as np
-
-class NeuralNetwork:
-    def __init__(self, x, y):
-        self.input      = x
-        self.weights1   = np.random.rand(self.input.shape[1],100)
-        self.weights2   = np.random.rand(100,1)
-        self.y          = y
-        self.output     = np.zeros(self.y.shape)
-
-    def feedforward(self):
-        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
-        self.output = sigmoid(np.dot(self.layer1, self.weights2))
-
-    def backprop(self):
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
-        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
-
-        # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
-
-# X = np.array([[0,0,1],
-#               [0,1,1],
-#               [1,0,1],
-#               [1,1,1]])
-# y = np.array([[0],[1],[1],[0]])
-nn = NeuralNetwork(x,y)
-
-
-
-for i in range(1500):
-    nn.feedforward()
-    nn.backprop()
-    print(rmse(y, nn.output))
-
-print(nn.output)
-
-
