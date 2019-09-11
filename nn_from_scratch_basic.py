df287e196f62f4348d51449b942519e6554c943e
Dev Sharma <devkosal@gmail.com>
2019-09-11 12:10:02 -0400

    Update and rename from_scracth.py to nn_from_scratch_basic.py

--- nn_from_scratch_basic.py
+++ nn_from_scratch_basic.py
@@ -1,6 +1,5 @@
 # https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 
-
 import numpy as np
 
 # def relu(x):
@@ -18,10 +17,6 @@ x = np.array([[1,2,3],
 
 y = np.array([[10],
              [24]])
-
-# e = np.array([[1,1,1],
-#               [1,2,1],
-#               [1,0,1]])
 
 
 class NueralNetwork:
